b1b7a05d5cc4438d7fd4fd145c501132f0616500
dvkscript <khoascript@gmail.com>
2025-06-24 14:51:25 +0700

    test

--- web/src/app/admin/interviews/page.tsx
+++ web/src/app/admin/interviews/page.tsx
@@ -1,20 +1,20 @@
 import { getAdminJobs } from "@/actions/admin.action";
-// import InterviewClient from "./InterviewClient";
+import InterviewClient from "./InterviewClient";
 
-interface InterviewsPageProps {
-  searchParams: Promise<{
-    page?: string;
-  }>
-}
+// interface InterviewsPageProps {
+//   searchParams: Promise<{
+//     page: string;
+//   }>
+// }
 
-export default async function InterviewsPage({ searchParams }: InterviewsPageProps) {
-  const { page = "1", ...rest } = await searchParams;
+export default async function InterviewsPage({ }) {
+  // const { page = "1", ...rest } = await searchParams;
 
   const limit = "10";
 
   const newSearchParams = {
-    page,
-    ...rest,
+    page: "1",
+    // ...rest,
     limit,
   }
 
@@ -24,8 +24,7 @@ export default async function InterviewsPage({ searchParams }: InterviewsPagePro
     throw new Error(res.status.toString())
   }
 
-  return <>test</>
-  // return (
-  //   <InterviewClient data={res.data} limit={limit} />
-  // )
+  return (
+    <InterviewClient data={res.data} limit={limit} />
+  )
 };
